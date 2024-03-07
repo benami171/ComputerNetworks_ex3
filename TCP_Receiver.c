7e66e5f03b2a7eb8e6d8f44f03d1e303d0d2d76a
Gal Ben Ami <102553622+benami171@users.noreply.github.com>
2024-03-07 11:44:55 +0200

    Add files via upload

--- TCP_Receiver.c
+++ TCP_Receiver.c
@@ -1,155 +1,146 @@
 #include <stdio.h> // Standard input/output library
 #include <arpa/inet.h> // For the in_addr structure and the inet_pton function
 #include <sys/time.h> // For the gettimeofday function
+#include <time.h> // For the time function
 #include <sys/socket.h> // For the socket function
 #include <netinet/in.h> // For the sockaddr_in structure
 #include <netinet/tcp.h> // for TCP_CONGESTION
 #include <unistd.h> // For the close function
 #include <string.h> // For the memset function
 #include <stdlib.h> // Standard library
-#include <unistd.h> // For the close function
-#define CLIENTS 1
-#define ADDR "127.0.0.1" // local host
-
-/*
- * @brief The TCP's receiver port.
- * @note The default port is 6000.
-*/
-#define PORT 6606
-
-/*
- * @brief The maximum number of clients that the receiver can handle.
- * @note The default maximum number of clients is 1.
-*/
-#define MAX_CLIENTS 1
-
-/*
- * @brief The buffer size to store the received message.
- * @note The default buffer size is 1024.
-*/
 #define BUFFER_SIZE 8192*4
+#define EXPECTED_BYTES 2097152
 
-/*
- * @brief TCP receiver main function.
- * @param None
- * @return 0 if the receiver runs successfully, 1 otherwise.
-*/
-int main(void) {
-    int total_bytes_received = 0;
-    int sock = -1;
+// This function is called whenever an error occurs.
+// It prints an error message, closes any open sockets, and then terminates the program.
+void handleError(int sock, int sender_socket, const char* msg) {
+    perror(msg);
+    if (sender_socket != -1) close(sender_socket);
+    if (sock != -1) close(sock);
+    exit(1);
+}
 
-    struct sockaddr_in receiver;
-    struct sockaddr_in sender;
-    // Stores the sender's structure length.
-    socklen_t sender_len = sizeof(sender);
+// function for setting up sockets while error handling using handleError helper function.
+int setupSocket(int receiver_port) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    // check if an error occured.
+    if (sock == -1) handleError(-1, -1, "socket(2)");
 
     int opt = 1;
-    memset(&receiver, 0, sizeof(receiver));
-    memset(&sender, 0, sizeof(sender));
-    // Try to create a TCP socket (IPv4, stream-based, default protocol).
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sock == -1)
-    {
-        perror("socket(2)");
-        return 1;
-    }
-
-    // Set the socket option to reuse the receiver's address.
-    // This is useful to avoid the "Address already in use" error message when restarting the receiver.
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
-    {
-        perror("setsockopt(2)");
-        close(sock);
-        return 1;
-    }
+        handleError(sock, -1, "setsockopt(2)");
 
-    receiver.sin_addr.s_addr = INADDR_ANY;
+    struct sockaddr_in receiver;
+    memset(&receiver, 0, sizeof(receiver));
+    receiver.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (receiver.sin_addr.s_addr == INADDR_NONE) 
+        handleError(sock, -1, "inet_addr(3)");
     receiver.sin_family = AF_INET;
-    receiver.sin_port = htons(PORT
-);
-
-    // Try to bind the socket to the receiver's address and port.
+    receiver.sin_port = htons(receiver_port);
+    
     if (bind(sock, (struct sockaddr *)&receiver, sizeof(receiver)) < 0)
-    {
-        perror("bind(2)");
-        close(sock);
-        return 1;
-    }
+        handleError(sock, -1, "bind(2)");
 
-    // Try to listen for incoming connections.
     if (listen(sock, 1) < 0)
+        handleError(sock, -1, "listen(2)");
+    // returns the socket descriptor to accept connections from clients.
+    return sock;
+}
+
+
+int main(int argc, char *argv[]) {
+    clock_t start_t, end_t;
+    double total_t;
+    double avgTime_t;
+    int times_sent = 0;
+    double total_bandwidth = 0;
+    if (argc != 5)
     {
-        perror("listen(2)");
-        close(sock);
+        fprintf(stderr, "5 arguments needed !\n");
         return 1;
     }
 
-    fprintf(stdout, "Listening for incoming connections on port %d...\n", PORT
-);
+    const int receiver_port = atoi(argv[2]);
+    const char *algo = argv[4];
 
-    // The receiver's main loop.
-    while (1)
+    struct sockaddr_in sender;
+    socklen_t sender_len = sizeof(sender);
+    // create a socket, bind it to the specified port, and start listening for incoming connections.
+    int sock = setupSocket(receiver_port);
+    fprintf(stdout, "Listening for incoming connections on port %d...\n", receiver_port);
+    // Blocks until a client connects to the Receiver.
+    int sender_socket = accept(sock, (struct sockaddr *)&sender, &sender_len);
+    // Sets congestion control algos, the user chooses which algo to use by input.
+    if (setsockopt(sender_socket, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0)
     {
-        // Try to accept a new sender connection.
-        int sender_socket = accept(sock, (struct sockaddr *)&sender, &sender_len);
+        handleError(sock, sender_socket, "setsockopt(2) - TCP_CONGESTION");
+    }
+    if (sender_socket < 0)
+    {
+        handleError(sock, -1, "accept(2)");
+    }
+    fprintf(stdout, "Sender connected, beginning to receive file..\n");
 
-        // If the accept call failed, print an error message and return 1.
-        if (sender_socket < 0)
-        {
-            perror("accept(2)");
-            close(sock);
-            return 1;
+
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
+
+    int bytes_received = 0;
+    int total_bytes_received = 0;
+    
+    start_t = clock();
+    while (total_bytes_received < EXPECTED_BYTES)
+    {
+        bytes_received = recv(sender_socket, buffer, BUFFER_SIZE, 0);
+        total_bytes_received += bytes_received;
+        // If the message receiving failed, print an error message and return 1.
+        if (bytes_received < 0){
+        perror("recv(2)");
+        close(sender_socket);
+        close(sock);
+        return 1;
+        } else if (bytes_received == 0){
+            fprintf(stdout, "Sender disconnected\n\n");
+            break;
         }
-        fprintf(stdout, "Client %s:%d connected\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port));
-
-        char buffer[BUFFER_SIZE] = {0};
-        struct timeval start, end;
-        gettimeofday(&start, NULL); // Get the current time before receiving the file
-        while (1){
-            int bytes_received = recv(sender_socket, buffer, BUFFER_SIZE, 0);
-
-            // If the message receiving failed, print an error message and return 1.
-            if (bytes_received < 0)
-            {
-            perror("recv(2)");
-            close(sender_socket);
-            close(sock);
-            return 1;
-            }
-            // If the amount of received bytes is 0, the sender has disconnected.
-            // Close the sender's socket and continue to the next iteration.
-            else if (bytes_received == 0)
-            {
-            fprintf(stdout, "Client %s:%d disconnected\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port));
-            break;
-            }
-            buffer[bytes_received] = '\0'; // null-terminate the received data
-            if (strcmp(buffer, "EXIT") == 0)
-            {
-            fprintf(stdout, "Received EXIT message from client %s:%d\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port));
-            break;
-            }
-
-            total_bytes_received += bytes_received;
+        
+        if (total_bytes_received>=EXPECTED_BYTES){
+            fprintf(stdout, "File transfer completed.\n");
+            times_sent++;
+            end_t = clock();
+            double time_taken = ((double)(end_t - start_t) / CLOCKS_PER_SEC) * 1000;
+            double singleBandwidth = ((double)total_bytes_received / (time_taken / 1000)) /(1024 * 1024);
+            fprintf(stdout, "Run# %d Data: Time=%.2f; Speed=%.2fMB/s \n", times_sent,time_taken, singleBandwidth);
+            
+            total_t += time_taken;
+            total_bandwidth += singleBandwidth;
+            total_bytes_received = 0;
+            singleBandwidth = 0;
+            time_taken = 0;
+            fprintf(stdout, "Waiting for a sender response...\n");
             
         }
-        gettimeofday(&end, NULL); // Get the current time after receiving the file
-
-        long time_taken = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec)) / 1000; // Calculate the time taken in milliseconds
-        double bandwidth = (double)total_bytes_received / time_taken * 1000; // Calculate the average bandwidth in bytes per second
-
-        fprintf(stdout, "Time taken to receive the file: %ld ms\n", time_taken);
-        fprintf(stdout, "Average bandwidth: %.2f bytes/s\n", bandwidth);
-       // Close the sender's socket and continue to the next iteration.
-        close(sender_socket);
-        fprintf(stdout, "Total bytes received from the sender %s:%d: %d\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port), total_bytes_received);
-        fprintf(stdout, "Client %s:%d disconnected\n", inet_ntoa(sender.sin_addr), ntohs(sender.sin_port));
+        buffer[bytes_received] = '\0'; // null-terminate the received data
+        if(strcmp(buffer, "yes") == 0) { // sender decided to send again
+            start_t = clock();
+        }
+        if (strcmp(buffer, "EXIT") == 0){
+        fprintf(stdout, "Sender sent EXIT message \n\n");
+        break;
+        }
     }
-
-    // Close the receiver's socket.
+    
+    /******************STATISTICS*************************/
+    avgTime_t = (total_t / times_sent);
+    fprintf(stdout, "------------------------------------------------\n\t\t * Statistics * \t\t\n\n");
+    fprintf(stdout, "- Average time: %.2f ms\n", avgTime_t);        
+    //double avgBandwidth = (((double)total_bandwidth*times_sent) / total_t); // Calculate the average bandwidth in megabytes per second
+    double avgBandwidth = (total_bandwidth / times_sent); // average bandwidth in MB/s
+    fprintf(stdout, "- Average bandwidth: %.2f MB/s\n", avgBandwidth);
+    fprintf(stdout, "------------------------------------------------\n");
+    // Close the sender's socket and continue to the next iteration.
+    close(sender_socket);
     close(sock);
-    fprintf(stdout, "Receiver finished!\n");
-
+    fprintf(stdout, "Receiver done.\n");
     return 0;
 }
--- TCP_Sender.c
+++ TCP_Sender.c
@@ -1,6 +1,7 @@
 #include <stdio.h>      // Standard input/output library
 #include <arpa/inet.h>  // For the in_addr structure and the inet_pton function
 #include <sys/socket.h> // For the socket function
+#include <netinet/tcp.h> // for TCP_CONGESTION
 #include <sys/time.h>   // For the gettimeofday function
 #include <unistd.h>     // For the close function
 #include <string.h>     // For the memset function
@@ -9,86 +10,62 @@
 
 #define RECEIVER_IP "127.0.0.1"
 #define RECEIVER_PORT 6606
-#define BUFFER_SIZE 8192 * 4
+#define FILE_SIZE 2097152
 
 char *util_generate_random_data(unsigned int size);
 
-int main(void){
-
-    unsigned int file_size = 1 << 21;
-    char *random_file = util_generate_random_data(file_size);
-    char *file_message = random_file;
-    unsigned int total_bytes_sent = 0;
-    unsigned int bytes_left = file_size;
-    int bytes_sent;
-    int sock = -1;
-
-    char buffer[BUFFER_SIZE + sizeof(struct timeval)] = {0};
-    struct timeval timestamp;
-
-
-    struct sockaddr_in receiver;
-    memset(&receiver, 0, sizeof(receiver));
-
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (sock == -1)
-    {
-        perror("socket(2)");
-        free(random_file);
+int main(int argc,char *argv[]){
+    if (argc != 7){
+        fprintf(stderr, "not enough arguments\n");
         return 1;
     }
+    const char *receiver_ip = argv[2];
+    const int receiver_port = atoi(argv[4]);
+    const char *algo = argv[6];
+    char *random_file = util_generate_random_data(FILE_SIZE);
 
-    if (inet_pton(AF_INET, RECEIVER_IP, &receiver.sin_addr) <= 0)
-    {
-        perror("inet_pton(3)");
+    int sock = -1;
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (setsockopt(sock, IPPROTO_TCP, TCP_CONGESTION, algo, strlen(algo)) < 0){
+        perror("setsockopt(2)");
         free(random_file);
         close(sock);
         return 1;
     }
 
+    if (sock == -1){
+        perror("socket(2)");
+        free(random_file);
+        return 1;
+    }
+    struct sockaddr_in receiver;
+    memset(&receiver, 0, sizeof(receiver));
     receiver.sin_family = AF_INET;
-    receiver.sin_port = htons(RECEIVER_PORT);
+    receiver.sin_port = htons(receiver_port);
 
-    fprintf(stdout, "Connecting to %s:%d...\n", RECEIVER_IP, RECEIVER_PORT);
+    if (inet_pton(AF_INET, receiver_ip, &receiver.sin_addr) <= 0){
+        perror("inet_pton(3)");
+        close(sock);
+        free(random_file);
+        return 1;
+    }
+
+    fprintf(stdout, "Connecting to %s:%d...\n", receiver_ip, receiver_port);
 
     // Try to connect to the receiver using the socket and the receiver structure.
-    if (connect(sock, (struct sockaddr *)&receiver, sizeof(receiver)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *)&receiver, sizeof(receiver)) < 0){
         perror("connect(2)");
         free(random_file);
         close(sock);
         return 1;
     }
-
     fprintf(stdout, "Successfully connected to the receiver!\n");
 
-    fprintf(stdout, "Trying to send random file...\n");
-
-    while (total_bytes_sent < file_size) {
-        bytes_sent = send(sock, file_message + total_bytes_sent, bytes_left, 0);
-        if (bytes_sent <= 0)
-        {
-            perror("send(2)");
-            close(sock);
-            return 1;
-        }
-        total_bytes_sent += bytes_sent;
-        bytes_left -= bytes_sent;
-    }
-    fprintf(stdout, "Sent %d bytes to the receiver!\n"
-                    "Waiting for the receiver to respond...\n",
-            bytes_sent);
-    fprintf(stdout, "User Decision: Send the file again? (y/n): ");
     char user_decision;
-    scanf("%c", &user_decision);
-    while (user_decision == 'y')
-{
-    total_bytes_sent = 0;
-    bytes_left = file_size;
-    file_message = random_file;
-    while (total_bytes_sent < file_size) {
-        bytes_sent = send(sock, file_message + total_bytes_sent, bytes_left, 0);
+    do{
+        fprintf(stdout, "Trying to send random file...\n");
+
+        int bytes_sent = send(sock, random_file, FILE_SIZE, 0);
         if (bytes_sent <= 0)
         {
             perror("send(2)");
@@ -96,28 +73,33 @@ int main(void){
             free(random_file);
             return 1;
         }
-        total_bytes_sent += bytes_sent;
-        bytes_left -= bytes_sent;
-    }
-    fprintf(stdout, "Sent %d bytes to the receiver!\n"
-                    "Waiting for the receiver to respond...\n",
-            bytes_sent);
-    fprintf(stdout, "User Decision: Send the file again? (y/n): ");
-    scanf(" %c", &user_decision); // Note the space before %c to skip any newline character
-}
-// send the string "exit" to the receiver.
+        fprintf(stdout, "Sent %d bytes to the receiver!\n"
+                        "User Decision: Send the file again? (y/n): ",bytes_sent);
+                        
+        scanf(" %c", &user_decision); // Note the space before %c to skip any newline character
+        
+        const char * spaceMessage = "yes";
+        if(send(sock,spaceMessage,strlen(spaceMessage),0) == -1){
+        perror("send(2)");
+        free(random_file);
+        close(sock);
+        return 1;
+        }
+        
+    } while (user_decision == 'y');
 
-fprintf(stdout, "User decided not to send the file again.\n");
-const char* exit_message = "EXIT";
-if (send(sock, exit_message, strlen(exit_message), 0) == -1) {
-    perror("send(2)");
+    // send the string "exit" to the receiver.
+
+    fprintf(stdout, "User decided not to send the file again.\n");
+    const char *exit_message = "EXIT";
+    if (send(sock, exit_message, strlen(exit_message), 0) == -1){
+        perror("send(2)");
+        free(random_file);
+        close(sock);
+        return 1;
+    }
     free(random_file);
     close(sock);
-    return 1;
-}
-free(random_file);
-close(sock);
-
 
     fprintf(stdout, "Connection closed!\n");
     return 0;
@@ -129,18 +111,13 @@ char *util_generate_random_data(unsigned int size)
     // Argument check.
     if (size == 0)
         return NULL;
-
     buffer = (char *)calloc(size, sizeof(char));
-
     // Error checking.
     if (buffer == NULL)
         return NULL;
-
     // Randomize the seed of the random number generator.
     srand(time(NULL));
-
     for (unsigned int i = 0; i < size; i++)
         *(buffer + i) = ((unsigned int)rand() % 256);
-
     return buffer;
 }
